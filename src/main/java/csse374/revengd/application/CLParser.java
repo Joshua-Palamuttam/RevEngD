5a64c1ef2aac8bd9ee72b9983c4ac289cfba460e
Joshua Alan Kuhn <kuhnja@rose-hulman.edu>
2017-12-06 00:05:39 -0500

    Added shell method and constructor to CLParser.

--- src/main/java/csse374/revengd/application/CLParser.java
+++ src/main/java/csse374/revengd/application/CLParser.java
@@ -1,5 +1,17 @@
 package csse374.revengd.application;
 
-public class CLParser {
+import java.awt.List;
 
+public class CLParser {
+	
+	List argables;
+
+	public CLParser(List args){
+		this.argables = args;
+	}
+	
+	public void parseAll(String[] args){
+		//TODO
+	}
+	
 }
