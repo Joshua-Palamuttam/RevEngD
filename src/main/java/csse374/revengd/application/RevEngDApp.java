b60d84b68b1c356316e11bde431aadee68c8a062
Joshua James Heidecker <heidecjj@rose-hulman.edu>
2017-12-07 21:18:02 -0500

    now outputs plantum png

--- src/main/java/csse374/revengd/application/RevEngDApp.java
+++ src/main/java/csse374/revengd/application/RevEngDApp.java
@@ -1,10 +1,16 @@
 package csse374.revengd.application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import net.sourceforge.plantuml.SourceStringReader;
 import soot.SootClass;
 
 public class RevEngDApp {
@@ -37,7 +43,20 @@ public class RevEngDApp {
 		rf.generateRelationships();
 		
 		umlg.generate(rf.getRelationships());
-		System.out.println(umlg.getPlantUMLText());
+	
+		
+		// output to image
+		SourceStringReader reader = new SourceStringReader(umlg.getPlantUMLText());
+		// Write the first image to "png"
+		try {
+			reader.generateImage(new File("./output/plantuml.png"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
