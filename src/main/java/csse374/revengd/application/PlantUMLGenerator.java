6bb6de90fc6bbcbd07f80c9fca2edf808fb2bb33
Joshua James Heidecker <heidecjj@rose-hulman.edu>
2017-12-21 18:12:23 -0500

    removed some unnecessary print statements

--- src/main/java/csse374/revengd/application/PlantUMLGenerator.java
+++ src/main/java/csse374/revengd/application/PlantUMLGenerator.java
@@ -17,12 +17,9 @@ public class PlantUMLGenerator extends Analyzable {
 	public void analyze(AnalyzableData data, OutputStream out) {
 		SourceStringReader reader = new SourceStringReader(data.getUmlText());
 		try {
-			Path filePath = Paths.get(System.getProperty("user.dir"), "build", "plantuml", "diagram.png");
-			Files.createDirectories(filePath.getParent());
-				
 			FileFormatOption option = new FileFormatOption(FileFormat.SVG, false);
 			DiagramDescription description = reader.outputImage(out, option);
-			System.out.println("UML diagram generated at: " + filePath.toString());
+			System.out.println("UML diagram generated");
 			System.out.println(description);
 		} catch (Exception e) {
 			System.err.println("Cannot create file to store the UML diagram.");
