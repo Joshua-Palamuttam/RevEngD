6b771fff94649b18686a2f008be1487ca1c362d9
Joshua James Heidecker <heidecjj@rose-hulman.edu>
2018-02-08 23:54:18 -0500

    fixed composition over inheritance detection

--- src/main/java/csse374/revengd/application/CompInheritanceDetector.java
+++ src/main/java/csse374/revengd/application/CompInheritanceDetector.java
@@ -3,14 +3,19 @@ package csse374.revengd.application;
 import java.io.OutputStream;
 import java.util.Collection;
 
+import soot.Scene;
+import soot.SootClass;
+
 public class CompInheritanceDetector extends Analyzable {
 	public final static String PATTERN = "inheritance";
 	public final static String SUBCLASS = "subclass";
 	public final static String SUPERCLASS = "superclass";
 	
+	private Scene scene;
+	
 	@Override
 	public void analyze(AnalyzableData data, OutputStream out) {
-		
+		this.scene = data.getScene();
 		Collection<Relationship> relationships = data.getRelationships();
 		relationships.forEach(r -> {
 			if(this.useFiltersOn(r.getThisClass()) && isInheritance(r)){
@@ -27,10 +32,18 @@ public class CompInheritanceDetector extends Analyzable {
 	}
 
 	private boolean isInheritance(Relationship r) {
-		 return  !(r.getExtendz() == null) 
-				&& (this.useFiltersOn(r.getExtendz()) 
-				&& !r.getExtendz().isAbstract());
-	
+		if (r.getExtendz() == null
+				|| !this.useFiltersOn(r.getThisClass())
+				|| !r.getThisClass().isConcrete()) {
+			return false;
+		}
+		
+		SootClass superClass = r.getExtendz();
+		
+		return !r.getThisClass().getMethods().stream()
+			.filter(m -> !m.isConstructor())
+			.filter(m -> superClass.declaresMethod(m.getSubSignature()))
+			.allMatch(m -> TypeResolver.methodBodyUsesField(m, superClass, this.scene));
 	}
 		
 	
